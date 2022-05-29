44aca536eb07f4b61a87149abffb5d81e92f6cd3
Johannes Schiel <j.schiel@unleashed-design.de>
2022-05-29 12:45:43 +0200

    add vitest

---
+++ src/js/main.test.js
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest';
+
+test('Einfacher Test', () => {
+	expect(true).toBeTruthy();
+});
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    root: "src",
-    build: {
-        emptyOutDir: true,
-        outDir: "../dist"
-    }
-})
+	test: {
+		include: ['**/js/*.test.js'],
+		globals: true,
+		reporters: 'verbose',
+	},
+	root: 'src',
+	build: {
+		emptyOutDir: true,
+		outDir: '../dist',
+	},
+});
